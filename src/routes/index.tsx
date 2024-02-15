b9c525e81ccf0be395691e6b910e069b2c638954
Brenley Dueck <brenleydueck@gmail.com>
2024-02-15 00:01:50 -0600

    Different file paths

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,9 +5,9 @@ export default function Index() {
         <>
             <h1>Hello World</h1>
             <FileComponent path="./tmp/solidHome.txt">
-                <FileComponent path="./tmp/solidHome.txt">
-                    <FileComponent path="./tmp/solidHome.txt" />
-                    <FileComponent path="./tmp/solidHome.txt" />
+                <FileComponent path="./tmp/solidHome2.txt">
+                    <FileComponent path="./tmp/solidHome3.txt" />
+                    <FileComponent path="./tmp/solidHome4.txt" />
                 </FileComponent>
             </FileComponent>
         </>
